9e6b983f33ffab423f30871aaf334897d5308e55
eslamabdallah74 <eslamabdallah301@gmail.com>
2022-10-02 09:11:42 +0200

    add api trait

---
+++ app/Trait/ApiTrait.php
@@ -0,0 +1,62 @@
+<?php
+namespace  App\Trait;
+
+
+trait ApiTrait {
+
+    public function errorField($field){
+        $massage = __($field.' required');
+        return response()->json([
+            "msg"    => $massage,
+            "status" => false
+        ]);
+    }
+    public function errorMessage($massage,$code){
+        return response()->json([
+            "msg"           => __($massage),
+            "status"        => false,
+            "status_code"   => $code
+        ]);
+    }
+    public function SuccessMessage($massage){
+        return response()->json([
+            "msg"           => __($massage),
+            "status"        => true,
+            "status_code"   => 200
+        ]);
+    }
+    public function returnSuccess($msg=""){
+        return response()->json([
+            'status'=>true,
+            "msg"=>$msg
+        ]);
+    }
+    public function returnData($key,$value,$msg="",array $extra=[]){
+        $data = [
+            $key=>$value
+        ];
+        if (!empty($extra)) {
+            foreach ($extra as $key => $value) {
+                $data[$key] = $value;
+            }
+        }
+        $data['status'] = true;
+        $data['status_code'] = 200;
+        $data['msg'] = $msg;
+        return response()->json($data);
+    }
+    public function returnFailData($key,$value,$msg="",array $extra=[]){
+        $data = [
+            $key=>$value,
+            'status'=>false,
+            "msg"=>$msg
+        ];
+        if (!empty($extra)) {
+            foreach ($extra as $key => $value) {
+                $data[$key] = $value;
+            }
+        }
+        return response()->json($data);
+    }
+
+}
